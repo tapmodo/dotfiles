e4bfdec1f07d21a5f695e795ae9140dd6d798b69
Tapmodo <khallman@tapmodo.com>
2014-09-10 16:31:33 -0400

    Updated, added localhost

--- iptables.sh
+++ iptables.sh
@@ -8,16 +8,17 @@ iptables -F
 ipset destroy Tapmodo_Hosts
 ipset create Tapmodo_Hosts hash:ip
 
-ipset add Tapmodo_Hosts 107.170.243.51	#Name:	apps.tapmodo.net
-ipset add Tapmodo_Hosts 192.241.225.252 #Name:	alum.tapmodo.net
-ipset add Tapmodo_Hosts 162.243.149.132 #Name:	balti.tapmodo.net
-ipset add Tapmodo_Hosts 192.241.213.102 #Name:	bay.tapmodo.net
-ipset add Tapmodo_Hosts 198.199.104.233 #Name:	chili.tapmodo.net
-ipset add Tapmodo_Hosts 192.241.234.61	#Name:	clove.tapmodo.net
-ipset add Tapmodo_Hosts 198.199.96.77	#Name:	cress.tapmodo.net
-ipset add Tapmodo_Hosts 50.57.43.175	#Name:	aji.tapmodo.net
-ipset add Tapmodo_Hosts 192.241.221.81	#Name:	tikka.tapmodo.net
-ipset add Tapmodo_Hosts 192.241.195.142	#Name:	contender-pbx.tapmodo.net
+ipset add Tapmodo_Hosts 127.0.0.1       #Name:  apps.tapmodo.net
+ipset add Tapmodo_Hosts 107.170.243.51  #Name:  apps.tapmodo.net
+ipset add Tapmodo_Hosts 192.241.225.252 #Name:  alum.tapmodo.net
+ipset add Tapmodo_Hosts 162.243.149.132 #Name:  balti.tapmodo.net
+ipset add Tapmodo_Hosts 192.241.213.102 #Name:  bay.tapmodo.net
+ipset add Tapmodo_Hosts 198.199.104.233 #Name:  chili.tapmodo.net
+ipset add Tapmodo_Hosts 192.241.234.61  #Name:  clove.tapmodo.net
+ipset add Tapmodo_Hosts 198.199.96.77   #Name:  cress.tapmodo.net
+ipset add Tapmodo_Hosts 50.57.43.175    #Name:  aji.tapmodo.net
+ipset add Tapmodo_Hosts 192.241.221.81  #Name:  tikka.tapmodo.net
+ipset add Tapmodo_Hosts 192.241.195.142 #Name:  contender-pbx.tapmodo.net
 
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
